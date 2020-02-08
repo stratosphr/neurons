9b2704ff753512a0ae367c36a20d6292e28ad76b
stratosphr <stratosphr@gmail.com>
2020-02-08 18:31:09 +0100

    Initialized neural network with number of inputs, neurons on each layer and number of outputs

--- src/core/NeuralNet.ts
+++ src/core/NeuralNet.ts
@@ -1,7 +1,32 @@
+import ActivationFunctions from '@/core/ActivationFunctions'
+import Letters             from '@/data/Letters'
+
 export default class NeuralNet {
+    private readonly inputs: number
+    private readonly layers: number[]
+    private readonly outputs: number
+    private readonly activation: (x: number, l?: number) => number
+    private readonly bias: number
 
     public static main(): void {
-        console.log('main')
+        const inputs = 5 * 8
+        const outputs = 26
+        const layers = [16, 16]
+        const net = new NeuralNet(inputs, layers, outputs)
+        net.input(Letters.a)
+    }
+
+    constructor(inputs: number, layers: number[], outputs: number, activation = ActivationFunctions.SIGMOID, bias = 0) {
+        this.activation = activation
+        this.bias = bias
+        this.inputs = inputs
+        this.layers = layers
+        this.outputs = outputs
+    }
+
+    public input(a: number[]): void {
+        console.log(this.activation(10, 5))
+        console.log(a)
     }
 
 }
